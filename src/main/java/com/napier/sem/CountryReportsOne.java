4cdecca32b4816ba4f0a1563d49992e52c45ce77
d-vermeulen <40537880@live.napier.ac.uk>
2022-02-25 11:33:43 +0000

    - Created project_structure_improvment
    - Added class "Country Reports 1"

---
+++ src/main/java/com/napier/sem/CountryReportsOne.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+import java.sql.Connection;
+
+public class CountryReportsOne {
+
+    /**
+     * Class Description
+     * This class contains the methods required to produce reports for issue #11 "Country Reports 1"
+     */
+
+    // All the countries in the world organised by largest population to smallest.
+    public void World(Connection con){
+
+    }
+
+    // All the countries in a continent organised by largest population to smallest.
+    public void Continent(Connection con){
+
+    }
+
+    // All the countries in a region organised by largest population to smallest.
+    public void Region(Connection con){
+
+    }
+}
