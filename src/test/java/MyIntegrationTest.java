6d380d2677f4b92dfb9a3ed0458dbdf1641aeb10
d-vermeulen <40537880@live.napier.ac.uk>
2022-03-21 16:34:22 +0000

    - Introduced Integration Tests

---
+++ src/test/java/MyIntegrationTest.java
@@ -0,0 +1,36 @@
+// note: removed - package com.napier.sem;
+import com.napier.sem.HelloWorld;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyIntegrationTest
+{
+    static HelloWorld app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new HelloWorld();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void testGetCity()
+    {
+        assertNotEquals(4,5);
+    }
+
+}
