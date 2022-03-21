355fd1e4cb8559f96654fb6c5f9e78888f8badd1
d-vermeulen <40537880@live.napier.ac.uk>
2022-03-21 15:48:07 +0000

    - Successfully Implemented Sample Unit Test

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MyTest
+{
+    // Add Unit Tests Here!
+
+    @Test
+    void SampleTest()
+    {
+        assertEquals(5, 5);
+    }
+
+}
